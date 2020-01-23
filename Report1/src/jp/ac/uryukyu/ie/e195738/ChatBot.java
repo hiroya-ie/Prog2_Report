f6e6e0c205364d7d3a3ae7c9d70ac2ff50209855
HIROYA CHINEN <e195738@ie.u-ryukyu.ac.jp>
2020-01-24 00:08:59 +0900

    [add]final process part

--- Report1/src/jp/ac/uryukyu/ie/e195738/ChatBot.java
+++ Report1/src/jp/ac/uryukyu/ie/e195738/ChatBot.java
@@ -9,24 +9,29 @@ class Judge {
         input = txt; // inputにユーザ入力を保存
         if (input.contains("何歳")) {
             System.out.println("16歳だよ！");
-            // 正常に読み込めたか出力して確認する。
         }
-        System.out.println("Your input is = " + input);
+        System.out.println("Your input is = " + input); // 正常に読み込めたか出力して確認
+        System.out.println("入力待ち……"); // 入力を促す説明文
     }
 }
 
 public class ChatBot {
     public static void main(String[] args) {
-        // write your code here
         System.out.println("akanechan: 「じゃ～ん！プロちゃんの驚く顔が見たい野々原茜ちゃん登場ーっ！ね、驚いた？驚いた？茜みたいなダイヤの原石を前にしてビックリしちゃった？」");
 
         Scanner in = new Scanner(System.in); // 標準入力から読み込むスキャナ
 
         System.out.println("入力待ち……"); // 入力を促す説明文
-        while (!(in.nextLine().equals("bye"))) {
-            new Judge(in.nextLine());
-            System.out.println("入力待ち……"); // 入力を促す説明文
+
+        int count = 0;
+        String txt = in.nextLine();
+        while (!(txt.equals("bye"))) {
+            count++;
+            new Judge(txt);
         }
+        count++;
+        System.out.println("茜ちゃん人形作って待ってるね！");
+        System.out.println("今日はプロちゃんとたった" + count + "回しか話してないよ？");
 
     }
 }
