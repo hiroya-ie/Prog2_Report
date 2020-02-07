89fdf4903cb88c5af42af06d6f05035772f75fea
hiroya-ie <57645774+hiroya-ie@users.noreply.github.com>
2020-02-08 00:55:08 +0900

    report7

---
+++ rearrange.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void print_array(int array[], int size);
+void abc(int data[], int out[], int size);
+
+void print_array(int array[], int size){
+    for (int i = 0; i < size; i++){
+        printf(" %d",array[i]);
+    }
+    printf("\n");
+}
+
+void sort(int data[], int out[], int size){
+    int count = 0;
+    for (int i = 0; i < size; i++){
+        for (int max_value = 0, j = 0; j < size; j++){
+            if (data[j] >= max_value && data[j] >= 0){
+                max_value = data[j];
+                count = j;
+            }
+        }
+        out[i] = data[count];
+        data[count] = -1;
+    }
+}
+
+int main(){
+    const int SIZE = 5;
+    int scores[SIZE] = {0, 60, 70, 100, 90};
+    int result[SIZE];
+
+    printf ("sources =");
+    print_array(scores, SIZE);
+
+    sort(scores, result, SIZE);
+
+    printf ("results =");
+    print_array(result, SIZE);
+
+    return 0;
+}
