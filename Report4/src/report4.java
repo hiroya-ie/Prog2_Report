0bcb97d04b7a43c63b305c1fa2d5220fea260f3f
HIROYA CHINEN <e195738@ie.u-ryukyu.ac.jp>
2020-02-08 10:58:12 +0900

    [add]exception processing

--- Report4/src/report4.java
+++ Report4/src/report4.java
@@ -1,7 +1,13 @@
 public class report4 {
+
     public static void main(String[] args) {
         String str = null;
 
-        str.length();
+        try {
+            str.length();
+        } catch (NullPointerException e) {
+            System.out.println("例外が発生しました: " + e);
+            System.out.println(e.getMessage());
+        }
     }
 }
